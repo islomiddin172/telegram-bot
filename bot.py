840e21032d4edd49dfc49d088efbbff0712cfa7e
islomiddin172 <gamerislombek@gmail.com>
2026-04-16 14:21:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,53 +4,54 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 import yt_dlp
 
-# TOKEN Railway'dan olinadi
 TOKEN = os.getenv("TOKEN")
+
+if not TOKEN:
+    raise ValueError("TOKEN topilmadi!")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-# /start komandasi
 @dp.message(CommandStart())
 async def start_handler(message: types.Message):
     await message.answer(
         "👋 Salom!\n\n"
-        "📥 Menga TikTok yoki Instagram havola yuboring 🔗\n\n"
-        "🎬 Men sizga videoni yuklab beraman 🚀"
+        "📥 TikTok yoki Instagram link yuboring\n"
+        "🎬 Men video yuklab beraman"
     )
 
-# link qabul qilish
 @dp.message()
 async def download_video(message: types.Message):
     url = message.text
 
     if "tiktok.com" not in url and "instagram.com" not in url:
-        await message.answer("❌ Iltimos, faqat TikTok yoki Instagram link yuboring")
+        await message.answer("❌ Faqat TikTok yoki Instagram link yuboring")
         return
 
-    await message.answer("⏳ Video yuklanmoqda...")
-
-    ydl_opts = {
-        'outtmpl': 'video.mp4'
-        'format': 'mp4'
-    }
+    await message.answer("⏳ Yuklanmoqda...")
 
     try:
+        ydl_opts = {
+            'format': 'mp4',
+            'outtmpl': 'video.mp4'
+        }
+
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
 
-       await message.answer_video(
-    types.FSInputFile("video.mp4"),
-            caption="📢 Mana sizning videoingiz!\n\n"
-                    "🔥 Do‘stlaringiz bilan ulashing!\n"
-                    "👉 Bizning botimiz: https://t.me/Reflexmbot"
+        video = types.FSInputFile("video.mp4")
+
+        await message.answer_video(
+            video,
+            caption="📢 Mana video!\n👉 Bot: https://t.me/Reflexmbot"
         )
 
     except Exception as e:
+        print("ERROR:", e)
         await message.answer("❌ Xatolik yuz berdi")
 
-# botni ishga tushirish
 async def main():
+    print("BOT ISHGA TUSHDI")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
